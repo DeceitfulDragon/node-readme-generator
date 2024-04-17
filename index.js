1288519528a87ef365b0ff6663fb7c2f27383e6d
CatGettingHitInTheFace <feartherenegade@gmail.com>
2024-04-17 09:00:27 -0500

    Finished(?) setup for the readme generation

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+// Badges for the licenses, more can be added if extra licenses are needed
+const licenseBadges = {
+    'MIT': '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)',
+    'GNU GPLv3': '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)',
+    'Apache License 2.0': '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)',
+    'ISC': '![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)'
+};
+
+
 // Questions for inquirer
 const questions = [    
 {
@@ -52,8 +61,48 @@ const questions = [
 }];
 
 // Stringing the readme together
-function generateREADME(answers) {
-
+function generateReadme(returnedAnswers) {
+    // This took way too much trial and error
+    // Probably could've found something online to copy and paste, then altered
+    return `
+    # ${returnedAnswers.projectTitle}
+    
+    ${licenseBadges[returnedAnswers.license]}
+    
+    ## Description
+    ${returnedAnswers.description}
+    
+    ## Table of Contents
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Testing](#tests)
+    - [Questions](#questions)
+    
+    ## Installation
+    \`\`\`
+    ${returnedAnswers.installation}
+    \`\`\`
+    
+    ## Usage
+    ${returnedAnswers.usage}
+    
+    ## License
+    This project is licensed under the ${returnedAnswers.license}
+    
+    ## Contributing
+    ${returnedAnswers.contribution}
+    
+    ## Testing
+    \`\`\`
+    ${returnedAnswers.tests}
+    \`\`\`
+    
+    ## Questions
+    For any questions, contact me @ [${returnedAnswers.email}](mailto:${returnedAnswers.email}).
+    My GitHub: [${returnedAnswers.githubUsername}](https://github.com/${returnedAnswers.githubUsername})
+    `;
 }
 
 
@@ -69,7 +118,7 @@ function init() {
     inquirer
         .prompt(questions)
         .then((answers) => {
-            const readmeContent = generateREADME(answers);
+            const readmeContent = generateReadme(answers);
             // File name, file content
             writeToFile('README.md', readmeContent);
         });
